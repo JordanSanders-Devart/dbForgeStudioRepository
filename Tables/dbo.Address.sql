d679aa695e064432b120949489e12918b32e6e3d
DenisS <DenisS@Devart.com>
2022-01-20 15:47:41 +0200

    First commit

---
+++ Tables/dbo.Address.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Address] (
+  [AddressID] [int] NULL,
+  [AddressLine1] [nvarchar](50) NULL,
+  [AddressLine2] [nvarchar](50) NULL,
+  [City] [nvarchar](50) NULL,
+  [StateProvinceID] [int] NULL,
+  [PostalCode] [nvarchar](50) NULL,
+  [SpatialLocation] [nvarchar](100) NULL,
+  [rowguid] [nvarchar](50) NULL,
+  [ModifiedDate] [datetime2] NULL
+)
+ON [PRIMARY]
+GO
---
+++ Tables/dbo.AddressType.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[AddressType] (
+  [AddressTypeID] [int] NULL,
+  [Name] [nvarchar](50) NULL,
+  [rowguid] [nvarchar](50) NULL,
+  [ModifiedDate] [datetime2] NULL
+)
+ON [PRIMARY]
+GO

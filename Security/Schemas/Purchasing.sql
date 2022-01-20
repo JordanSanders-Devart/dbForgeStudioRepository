d70b486c1e76e1cb7d2a14569c9633f543b3a145
JordanSanders-Devart <62429421+JordanSanders-Devart@users.noreply.github.com>
2022-01-20 14:14:52 +0200

    Delete Security/Schemas directory

--- Security/Schemas/Purchasing.sql
+++
@@ -1,5 +0,0 @@
-﻿CREATE SCHEMA [Purchasing] AUTHORIZATION [dbo]
-GO
-
-EXEC sys.sp_addextendedproperty N'MS_Description', N'Contains objects related to vendors and purchase orders.', 'SCHEMA', N'Purchasing'
-GO

faf9df294700d2911670aa71cebaf2b32f9ed5a8
JordanSanders-Devart <62429421+JordanSanders-Devart@users.noreply.github.com>
2022-01-25 10:22:07 +0200

    Delete Tables directory

--- Tables/dbo.AddressType.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[AddressType] (
-  [AddressTypeID] [int] NULL,
-  [Name] [nvarchar](50) NULL,
-  [rowguid] [nvarchar](50) NULL,
-  [ModifiedDate] [datetime2] NULL
-)
-ON [PRIMARY]
-GO

e944e3c628177fc9fb66105464bc64338dbfbe47
JordanSanders-Devart <62429421+JordanSanders-Devart@users.noreply.github.com>
2023-11-21 11:05:25 +0200

    Delete Security/Schemas directory

--- Security/Schemas/HumanResources.sql
+++
@@ -1,5 +0,0 @@
-﻿CREATE SCHEMA [HumanResources] AUTHORIZATION [dbo]
-GO
-
-EXEC sys.sp_addextendedproperty N'MS_Description', N'Contains objects related to employees and departments.', 'SCHEMA', N'HumanResources'
-GO

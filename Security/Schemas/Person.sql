6aa09e992dc390d27cb0ece70447fa3974dacda2
JordanSanders-Devart <62429421+JordanSanders-Devart@users.noreply.github.com>
2024-06-21 09:55:57 +0200

    Delete Security/Schemas directory

--- Security/Schemas/Person.sql
+++
@@ -1,5 +0,0 @@
-﻿CREATE SCHEMA [Person] AUTHORIZATION [dbo]
-GO
-
-EXEC sys.sp_addextendedproperty N'MS_Description', N'Contains objects related to names and addresses of customers, vendors, and employees', 'SCHEMA', N'Person'
-GO

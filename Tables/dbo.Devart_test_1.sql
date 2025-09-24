9c2089a3a0bd7e05998dd445b5904fa4e176adce
JordanS <JordanSanders@gmail.com>
2025-09-24 17:22:36 +0200

    NEW COMMIT 1

---
+++ Tables/dbo.Devart_test_1.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Devart_test_1] (
+  [ID] [nchar](50) NULL,
+  [Name] [nchar](50) NULL,
+  [Email] [nchar](50) NULL
+)
+ON [PRIMARY]
+GO
---
+++ Tables/dbo.dept.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[dept] (
+  [deptno] [int] NOT NULL,
+  [dname] [varchar](14) NULL,
+  [loc] [varchar](13) NULL,
+  CONSTRAINT [PK_dept] PRIMARY KEY CLUSTERED ([deptno])
+)
+ON [PRIMARY]
+GO

6c5fe15c520e4a5d7d5d54975eca1067eb4236c6
Jordan <DevartDemo@gmail.com>
2026-06-24 17:49:10 +0300

    StaticData

---
+++ Tables/StaticData/dbo.Employee.Data.sql
@@ -0,0 +1,5 @@
+﻿INSERT dbo.Employee(ID, Name) VALUES (1, 'John');
+INSERT dbo.Employee(ID, Name) VALUES (2, 'Alice');
+INSERT dbo.Employee(ID, Name) VALUES (3, 'Michael');
+INSERT dbo.Employee(ID, Name) VALUES (4, 'Emma');
+INSERT dbo.Employee(ID, Name) VALUES (5, 'David');

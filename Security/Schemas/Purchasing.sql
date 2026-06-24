4d6a4808886ebe2dc7913b4879d16d6f2643defe
JordanSanders-Devart <62429421+JordanSanders-Devart@users.noreply.github.com>
2026-06-24 18:27:07 +0300

    Delete Security/Schemas directory

--- Security/Schemas/Purchasing.sql
+++
@@ -1,5 +0,0 @@
-﻿CREATE SCHEMA [Purchasing]
-GO
-
-EXEC sys.sp_addextendedproperty N'MS_Description', N'Contains objects related to vendors and purchase orders.', 'SCHEMA', N'Purchasing'
-GO
--- Security/Schemas/Sales.sql
+++
@@ -1,5 +0,0 @@
-﻿CREATE SCHEMA [Sales]
-GO
-
-EXEC sys.sp_addextendedproperty N'MS_Description', N'Contains objects related to customers, sales orders, and sales territories.', 'SCHEMA', N'Sales'
-GO
